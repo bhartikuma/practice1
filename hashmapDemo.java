4204c5454d916351597105b78d93ffdbe79f3ede
Bharti Kumari <149286231+bhartikuma@users.noreply.github.com>
2026-01-24 11:30:40 +0530

    Add files via upload

---
+++ hashmapDemo.java
@@ -0,0 +1,15 @@
+package collection;
+import java.util.*;
+
+public class hashmapDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String,Integer> map = new HashMap<>();
+		map.put("java", 1);
+		map.put("java", 2);
+		System.out.println(map.get("java"));
+
+	}
+
+}
